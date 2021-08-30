d77700186f4156a4b45c0c7c6965b6be50f36315
Gurdeep <saini.gurdeep1688@gmail.com>
2021-08-30 16:42:27 +0530

    Fragment -- Bottom Nav

---
+++ app/src/main/java/com/timeepass/project/letsconnect/Fragments/RequestFragment.java
@@ -0,0 +1,23 @@
+package com.timeepass.project.letsconnect.Fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.timeepass.project.letsconnect.R;
+
+public class RequestFragment extends Fragment
+{
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.requestfragment, container,false);
+
+        return view;
+    }
+}
--- app/src/main/java/com/timeepass/project/letsconnect/MainActivity.java
+++ app/src/main/java/com/timeepass/project/letsconnect/MainActivity.java
@@ -1,13 +1,22 @@
 package com.timeepass.project.letsconnect;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.timeepass.project.letsconnect.Fragments.AskFragment;
+import com.timeepass.project.letsconnect.Fragments.HomeFragment;
+import com.timeepass.project.letsconnect.Fragments.ProfileFragment;
+import com.timeepass.project.letsconnect.Fragments.RequestFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,7 +28,41 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         auth = FirebaseAuth.getInstance();
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav);
+        bottomNavigationView.setOnNavigationItemSelectedListener(onNav);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, new HomeFragment()).commit();
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener onNav = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @SuppressLint("NonConstantResourceId")
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+
+            Fragment selected = null;
+            switch(item.getItemId())
+            {
+                case R.id.home_bottom:
+                    selected = new HomeFragment();
+                    break;
+                case R.id.ask_bottom:
+                    selected = new AskFragment();
+                    break;
+                case R.id.request_bottom:
+                    selected = new RequestFragment();
+                    break;
+                case R.id.profile_bottom:
+                    selected = new ProfileFragment();
+                    break;
+            }
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, selected).commit();
+
+            return true;
+        }
+    };
 
     public void logout(View view) {
         auth.signOut();
