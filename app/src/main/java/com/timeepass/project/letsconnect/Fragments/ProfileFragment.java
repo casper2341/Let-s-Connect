2fe9e47beb7f954768de25ae4af8748cc8b81bad
Gurdeep <saini.gurdeep1688@gmail.com>
2021-09-04 02:56:51 +0530

    XML and Retrive - UserProfile

--- app/src/main/java/com/timeepass/project/letsconnect/Fragments/ProfileFragment.java
+++ app/src/main/java/com/timeepass/project/letsconnect/Fragments/ProfileFragment.java
@@ -5,17 +5,31 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.squareup.picasso.Picasso;
 import com.timeepass.project.letsconnect.R;
 import com.timeepass.project.letsconnect.UserProfile.CreateProfile;
 
+import org.w3c.dom.Text;
+
 public class ProfileFragment extends Fragment implements View.OnClickListener
 {
+    ImageView imageView;
+    TextView nameEt, profEt, bioEt, emailEt, webEt;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -28,9 +42,14 @@ public class ProfileFragment extends Fragment implements View.OnClickListener
     {
         super.onActivityCreated(savedInstanceState);
 
-        TextView textView = getActivity().findViewById(R.id.tv_prof);
+         imageView = getActivity().findViewById(R.id.iv_proffrag);
+         nameEt = getActivity().findViewById(R.id.tv_name_proffrag);
+         profEt = getActivity().findViewById(R.id.tv_prof_proffrag);
+         bioEt = getActivity().findViewById(R.id.tv_bio_proffrag);
+         emailEt = getActivity().findViewById(R.id.tv_email_proffrag);
+         webEt = getActivity().findViewById(R.id.tv_web_proffrag);
 
-        textView.setOnClickListener(this);
+
     }
 
     @Override
@@ -38,9 +57,49 @@ public class ProfileFragment extends Fragment implements View.OnClickListener
 
         switch (view.getId())
         {
-            case R.id.tv_prof:
-            Intent intent = new Intent(getActivity(), CreateProfile.class);
-            startActivity(intent);
+
         }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        String currentId = user.getUid();
+        DocumentReference reference;
+        FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+
+        reference = firestore.collection("user").document(currentId);
+
+        reference.get()
+                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+
+                        if(task.getResult().exists())
+                        {
+                            String nameResult = task.getResult().getString("name");
+                            String bioResult = task.getResult().getString("bio");
+                            String emailResult = task.getResult().getString("email");
+                            String webResult = task.getResult().getString("web");
+                            String urlResult = task.getResult().getString("url");
+                            String profResult = task.getResult().getString("prof");
+
+                            Picasso.get().load(urlResult).into(imageView);
+                            nameEt.setText(nameResult);
+                            bioEt.setText(bioResult);
+                            emailEt.setText(emailResult);
+                            webEt.setText(webResult);
+                            profEt.setText(profResult);
+                        }
+                        else
+                        {
+                            Intent intent = new Intent(getActivity(), CreateProfile.class);
+                            startActivity(intent);
+                        }
+                    }
+                });
     }
 }
